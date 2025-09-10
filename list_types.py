a0d8679780420883e9b3d3617e9c71fdb570702e
Johane Muller <johanemuller123@gmail.com>
2025-09-10 20:58:22 +0200

    Initial commit

---
+++ list_types.py
@@ -0,0 +1,24 @@
+#Create list with names of three friends
+friends_names = ["Ansune du Plessis", "Wilri Jacobs", "Mari van der Berg"]
+print(friends_names)
+
+#Print out name of first friend then the name of the last friend
+print(friends_names[0])
+print(friends_names[-1])
+
+#Print out name of first friend then the name of the last friend - combined
+print(friends_names[0:-0])
+
+# Print the length of the list
+print(len(friends_names))
+
+#Store friends age
+friends_ages = [33, 25, 40]
+
+#Print name and age of each friend
+print(f"{friends_names[-1]} is {friends_ages [-1]} years old.")
+
+
+
+
+
